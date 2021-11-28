702410599f04670d8fb06f208a4c4297c0dfa304
linaben16 <lina12benfradj@gmail.com>
2021-11-28 15:36:04 +0100

    first commit

---
+++ src/navnar.js
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>welcome</h1>
+            <div className="links"></div>
+            <a href="/">home</a>
+            <a href="/">about </a>
+
+        </nav>
+
+
+    );
+}
+
+export default Navbar;
